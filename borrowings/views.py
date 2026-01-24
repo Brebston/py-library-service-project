a7d3dc321c258e8e84e55dc071522f7885237292
Yurii Palahitskyi <brebston7@gmail.com>
2026-01-24 11:36:06 +0100

    Add filtering for the Borrowings List endpoint

--- borrowings/views.py
+++ borrowings/views.py
@@ -15,16 +15,30 @@ class BorrowingViewSet(
     mixins.CreateModelMixin,
     viewsets.GenericViewSet,
 ):
+    queryset = Borrowing.objects.select_related("book", "user")
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
-        queryset = Borrowing.objects.select_related("book", "user")
+        queryset = self.queryset
         user = self.request.user
+        params = self.request.query_params
 
         if user.is_staff:
-            return queryset
+            user_id = params.get("user_id")
+            if user_id:
+                queryset = queryset.filter(user_id=user_id)
+        else:
+            queryset = queryset.filter(user=user)
 
-        return queryset.filter(user=user)
+        is_active = params.get("is_active")
+        if is_active is not None:
+            val = is_active.strip().lower()
+            if val in ("true", "1", "yes"):
+                queryset = queryset.filter(actual_return_date__isnull=True)
+            elif val in ("false", "0", "no"):
+                queryset = queryset.filter(actual_return_date__isnull=False)
+
+        return queryset
 
     def get_serializer_class(self):
         if self.action == "create":
